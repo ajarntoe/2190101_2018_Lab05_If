2306e1e8183c8aa661db2bb78edfc09c71105240
Vishnu Kotrajaras <ajarntoe@gmail.com>
2018-09-05 23:00:17 +0700

    Add MyMath.java

---
+++ src/MyMath.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MyMath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter value of x.");
+		double x = s.nextDouble();
+		System.out.println("Enter value of y.");
+		double y = s.nextDouble();
+		double result;
+
+		if (x >= -5 && x <= 5 && y >= 5) {
+			result = x * y;
+		} else if (x >= -5 && x <= 5 && y < 5) {
+			result = 0;
+		} else if (x > 5) {
+			result = x * x * y;
+		} else {
+			result = y * y * x;
+		}
+		System.out.println("The result is " + result + ".");
+
+	}
+
+}
