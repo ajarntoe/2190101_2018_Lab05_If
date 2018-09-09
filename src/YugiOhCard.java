0c47ea6fb80ec33895b9af54e7e06e5464ba44b3
Vishnu Kotrajaras <ajarntoe@gmail.com>
2018-09-09 16:59:10 +0700

    Add student file for TestYugi and YugiohCard

---
+++ src/YugiOhCard.java
@@ -0,0 +1,23 @@
+
+public class YugiOhCard {
+	String name;
+	int level;
+	int attack;
+	int defence;
+
+	public YugiOhCard(String name, int level, int attack, int defence) {
+		super();
+		this.name = name;
+		this.level = level;
+		this.attack = attack;
+		this.defence = defence;
+	}
+
+	public boolean equals(Object o) {
+		YugiOhCard o2 = (YugiOhCard) o;
+		return true; //remove this line
+		// fill in the rest of the code
+
+	}
+
+}
